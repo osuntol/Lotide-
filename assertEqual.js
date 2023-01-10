1a1c134d731579406fe5b90bab2357bdba0f6df9
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-01-10 23:42:08 +0000

    Pass or Fail Assertion

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+function assertEqual(actual, expected) {
+  if (actual === expected) {
+    console.log("🤩Assertion Passed:" + actual + " === " + expected);
+  } else {
+    console.log("😱Assertion Failed:" + actual  + " !== " + expected);
+  }
+}
+
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1,11);
+
