5c23f45825c64e7df6fe24ac1099c89f89ec9190
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-01-16 06:24:03 +0000

    Counting letters in a string

---
+++ countLetters.js
@@ -0,0 +1,30 @@
+function assertEqual(actual, expected) {
+  if (actual === expected) {
+    console.log(`🤩Assertion Passed: ${actual}   ===  ${expected}`);
+  } else {
+    console.log(`😱Assertion Failed  ${actual} !==  ${expected}`);
+  }
+}
+
+
+let stringUse = "lighthouse in the house"
+function countLetters(string) {
+  stringCount = {}
+  for (let i = 0; i <= string.length - 1; i++) {
+    let character = string[i];
+    
+    if (character === ' '){
+      continue
+  }
+    let regEx = new RegExp( character, "g")
+    
+    let foundLetters = string.match(regEx)
+    
+    stringCount[character] = foundLetters.length 
+} 
+  return stringCount
+}
+
+
+
+
