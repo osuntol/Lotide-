ad8d041c7cb1e1898b3ec315501b145b1792e9fc
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-01-16 23:49:32 +0000

    Finding key by value

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+function assertEqual(actual, expected) {
+  if (actual === expected) {
+    console.log(`🤩Assertion Passed: ${actual}   ===  ${expected}`);
+  } else {
+    console.log(`😱Assertion Failed  ${actual} !==  ${expected}`);
+  }
+}
+
+function findKeyByValue(obj, value) {
+  for (let key in obj) {
+    if (obj[key] === value){
+      return key
+    }
+} 
+return undefined 
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
