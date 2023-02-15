9e13e967c7e9118d904cf643302e07d847b2f4c0
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-02-15 19:47:23 +0000

    Assert arrays equal refactored

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,7 @@
 
-function eqArrays(array1, array2) {
-  if (array1.length !== array2.length){
-    return false
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false
-    } 
-  }
- return true;
-}
 
-function assertArrayEqual(array1, array2) {
+const eqArrays = require('./eqArrays')
+function assertArraysEqual(array1, array2) {
   if (eqArrays(array1, array2)) {
     console.log(`🤩Assertion Passed: ${array1}   ===  ${array2}`);
   } else {
@@ -20,5 +10,6 @@ function assertArrayEqual(array1, array2) {
 }
 
 
-assertArrayEqual([1, 2, 3], [1, 2, 3]) 
-assertArrayEqual([1, 2, 3], [3, 2, 1])
+
+
+module.exports = assertArraysEqual
