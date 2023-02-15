825813f00548349bbf1c85226bf8a80eb3c1fa1c
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-02-15 20:04:55 +0000

    Middle refactored

--- middle.js
+++ middle.js
@@ -1,45 +1,3 @@
-//ASSERT ARRAY EQUAL
-function eqArrays(array1, array2) {
-  if (array1.length !== array2.length){
-    return false
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false
-    } 
-  }
- return true;
-}
-
-function assertArrayEqual(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`🤩Assertion Passed: ${array1}   ===  ${array2}`);
-  } else {
-    console.log(`😱Assertion Failed  ${array1} !==  ${array2}`);
-  }
-}
-
-//EQ ARRAYS
-function assertEqual(actual, expected) {
-  if (actual === expected) {
-    console.log(`🤩Assertion Passed: ${actual}   ===  ${expected}`);
-  } else {
-    console.log(`😱Assertion Failed  ${actual} !==  ${expected}`);
-  }
-}
-
-function eqArrays(array1, array2) {
-  if (array1.length !== array2.length){
-    return false
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false
-    } 
-  }
- return true;
-}
-
 function middle (array) {
   if (array.length <= 2){
     return []
@@ -54,4 +12,5 @@ function middle (array) {
   }
 }
 
-console.log(middle([]))
+
+module.exports = middle;
