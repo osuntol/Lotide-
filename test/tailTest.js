908a9a11a504ad41483026ccac96b8be98af2127
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-02-15 20:36:40 +0000

    Tail test mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -3,5 +3,12 @@ const assertEqual = require('../assertEqual');
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 const result = tail(words);
+const assert = require('chai').assert;
 
 console.log(assertEqual(result.length, 2));
+
+describe("#tail", () => {
+  it("returns 2 for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(result, ['Lighthouse', 'Labs']);
+  });
+});
