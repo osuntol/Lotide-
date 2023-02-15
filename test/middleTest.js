7ff80c29011d10d995209f59e757c2bcf3750252
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-02-15 20:35:45 +0000

    Middle mocha and chai test

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,11 @@
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
 const eqArrays = require('../eqArrays');
+const assert = require('chai').assert;
 console.log(assertArraysEqual(middle([3,33,4,5,6,6]), [4,5]));
+
+describe("#middle", () => {
+  it("returns [4,5] for [3,33,4,5,6,6]", () => {
+    assert.deepEqual(middle([3,33,4,5,6,6]), [4,5]);
+  });
+});
