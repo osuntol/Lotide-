7657d9f68d22b96e7de7971d6a29a4e4348ca93c
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-02-15 20:22:09 +0000

    Head refractored

--- head.js
+++ head.js
@@ -1,10 +1,10 @@
-const assertEqual = require('./assertEqual')
+
 
 function head (array) {
   var first = array[0]
   return first
 }
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 
+
+module.exports = head;
